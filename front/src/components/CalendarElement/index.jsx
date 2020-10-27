8578b14b76374dbc7e98f4b6c156772a3d226015
ToshifumiIde <toshifumiide198765@gmail.com>
2020-10-28 06:24:12 +0900

    "今日の日付に青丸追加、今月以外をグレーダウン"

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import * as styles from "./style.css";
 import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
 
 const CalendarElement = ({ day }) =>{
   const isFirstDay = day.date() === 1; //文字列のフォーマットの設定
   const format = isFirstDay ? "M月D日" : "D" ; //月初めだけ、月情報を記載
+  
+  //当日かどうかの判断//
+  const today = dayjs();
+  const compareFormat = "YYYYMMDD";
+  const isToday = day.format(compareFormat) === today.format(compareFormat);
+
+  //今月以外をグレーダウン//
+  const isCurrentMonth = day.month() === today.month();
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
   return (
     <div className={styles.element}>
       <Typography 
-        className={styles.date}
         align="center"
-        variant="caption"
+        className={styles.date}
+        color={textColor}
         component="div"
+        variant="caption"
         >
+          <span className={isToday ? styles.today :""}>
     { day.format(format) }
+          </span>
       </Typography>
     </div>
   );
