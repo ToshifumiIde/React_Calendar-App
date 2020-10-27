efa2744db4194a25e7e4a555fa50f7ebd6b8e228
ToshifumiIde <toshifumiide198765@gmail.com>
2020-10-28 06:38:16 +0900

    カレンダーの外見のリファクタリング calendar.jsにisSameDay(),isSameMonth(),isFirstDay()を移行

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,19 +2,32 @@ import React from "react";
 import * as styles from "./style.css";
 import { Typography } from "@material-ui/core";
 import dayjs from "dayjs";
+import {isSameMonth , isSameDay , isFirstDay} from "../../services/calendar";
 
 const CalendarElement = ({ day }) =>{
-  const isFirstDay = day.date() === 1; //文字列のフォーマットの設定
-  const format = isFirstDay ? "M月D日" : "D" ; //月初めだけ、月情報を記載
+  // const isFirstDay = day.date() === 1; //文字列のフォーマットの設定
+  // const format = isFirstDay ? "M月D日" : "D" ; //月初めだけ、月情報を記載
   
-  //当日かどうかの判断//
-  const today = dayjs();
-  const compareFormat = "YYYYMMDD";
-  const isToday = day.format(compareFormat) === today.format(compareFormat);
+  // //当日かどうかの判断//
+  // const today = dayjs();
+  // const compareFormat = "YYYYMMDD";
+  // const isToday = day.format(compareFormat) === today.format(compareFormat);
 
-  //今月以外をグレーダウン//
-  const isCurrentMonth = day.month() === today.month();
-  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+  // //今月以外をグレーダウン//
+  // const isCurrentMonth = day.month() === today.month();
+  // const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
+  const today = dayjs();
+
+  //今月以外をグレーダウンする
+  const isCurrentMonth = isSameMonth(day,today);
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary" ;
+
+  //文字列のフォーマットの決定（月初めだけ、月情報を記載）
+  const format = isFirstDay(day) ? "M月D日" : "D" ;
+
+  //当日かどうかの判断
+  const isToday = isSameDay(day,today);
 
   return (
     <div className={styles.element}>
