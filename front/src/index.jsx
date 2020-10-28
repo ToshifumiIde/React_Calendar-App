c2ffacb83b5d68e9f24f5a16fce51999bb39c7f6
ToshifumiIde <toshifumiide198765@gmail.com>
2020-10-29 00:49:42 +0900

    DatePickerの導入を次回実行（現在未実行）

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-// import CalendarBoard from "./components/CalendarBoard";
 import CalendarBoard from "./components/CalendarBoard/container";
 import Navigation from "./components/Navigation/container";
+
+//3-5から開始
+import DayjsUtils from "@date-io/dayjs";
 
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -4,5 +4,3 @@ import calendarReducer from "./calendar/reducer";
 const rootReducer = combineReducers({calendar:calendarReducer});
 
 export default rootReducer;
-
-//3-2-1までコーディング完了
