41ca71cb100615ee1be00a0d55036f96cd0bf6e0
ToshifumiIde <toshifumiide198765@gmail.com>
2020-10-28 21:10:15 +0900

    カレンダーのツールバーを追加し、余白を調整

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 // import CalendarBoard from "./components/CalendarBoard";
 import CalendarBoard from "./components/CalendarBoard/container";
+import Navigation from "./components/Navigation/container";
 
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
@@ -17,6 +18,7 @@ const store = createStore(rootReducer);
 const App = () => {
   return(
     <Provider store={store}>
+      <Navigation />
       <CalendarBoard />
     </Provider>
   )
