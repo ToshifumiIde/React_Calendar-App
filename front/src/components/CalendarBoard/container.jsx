83495bd60332c2426d0d867e175a6744e88473f1
ToshifumiIde <toshifumiide198765@gmail.com>
2020-11-01 20:09:47 +0900

    dialogの作成（途中、要修正）

--- front/src/components/CalendarBoard/container.jsx
+++ front/src/components/CalendarBoard/container.jsx
@@ -1,14 +1,25 @@
 import { connect } from "react-redux";
 import CalendarBoard from "./presentation";
 import { createCalendar } from "../../services/calendar";
+import { addScheduleOpenDialog } from "../../redux/addSchedule/actions";
+
+const mapDispatchToProps = dispatch =>({
+  openAddScheduleDialog:()=> {
+    dispatch(addScheduleOpenDialog());
+  }
+});
 
 const mapStateToProps = state => ({calendar:state.calendar});
 
 const mergeProps = (stateProps , dispatchProps ) =>({
+  ...stateProps,
+  ...dispatchProps,
   month:stateProps.calendar,
   calendar:createCalendar(stateProps.calendar),
 });
 
-export default connect(mapStateToProps,null,mergeProps)(CalendarBoard);
-
-//3-2-5まで完了
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(CalendarBoard);
